4d6e8577685410fc10cd9fad0928317be7c27b71
PARTH-200 <153612770+PARTH-200@users.noreply.github.com>
2025-07-04 23:18:56 +0530

    Add failing test for negative numbers

--- incubyte_calculator_string/src/test/java/com/example/incubyte_calculator_string/IncubyteCalculatorStringApplicationTests.java
+++ incubyte_calculator_string/src/test/java/com/example/incubyte_calculator_string/IncubyteCalculatorStringApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class IncubyteCalculatorStringApplicationTests {
@@ -48,4 +49,20 @@ class IncubyteCalculatorStringApplicationTests {
 		assertEquals(3, calculator.add("//;\n1;2"));
 		assertEquals(10, calculator.add("//|\n1|2|3|4"));
 	}
+
+	@Test
+	public void should_throw_exception_for_negative_numbers() {
+		StringCalculator calculator = new StringCalculator();
+
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+			calculator.add("1,-2,3");
+		});
+		assertEquals("Negative numbers not allowed: -2", exception.getMessage());
+
+		exception = assertThrows(IllegalArgumentException.class, () -> {
+			calculator.add("1,-2,-3");
+		});
+		assertEquals("Negative numbers not allowed: -2, -3", exception.getMessage());
+	}
+
 }
